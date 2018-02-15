fd12cd47ba79187be6ba4ef46c1bb7e0ff193b08
yawming <yawming@users.noreply.github.com>
2018-02-15 11:27:04 -0800

    Update apicem_config.py
    new dns sandboxapicem.cisco.com

--- path-trace-labs/apicem_config.py
+++ path-trace-labs/apicem_config.py
@@ -1,8 +1,8 @@
 # Step 1
 # Change apic-em IP to the one you are using
-# APICEM_IP =  "sandboxapic.cisco.com"
+# APICEM_IP =  "sandboxapicem.cisco.com"
 # APICEM_IP is a string
-APICEM_IP = "devnetapi.cisco.com/sandbox/apic_em"
+APICEM_IP = "sandboxapicem.cisco.com"
 
 # Step 2
 # Eneter user name and password to get a service ticket
